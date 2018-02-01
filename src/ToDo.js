4d7a15401eb28cb1b858606432630f3c35e14326
Linda O'Neill <oneill.linda21@gmail.com>
2018-02-01 17:21:26 -0600

    react-events

---
+++ src/ToDo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+ class ToDo extends Component {
+   render() {
+     return (
+       <li>
+         <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+         <input type="button" value="Delete" onClick={ this.props.deleteToDo } />
+         <span>{this.props.description}</span>
+       </li>
+     );
+   }
+ }
+
+ export default ToDo;
